6d0bc3b4d6c3d4ad90653449fb524cd243e12306
Wyatt McMurry <wyattmcmurry1@gmail.com>
2021-10-16 15:49:53 -0400

    dropdown for switching between ellipse and square shapes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { Nest } from './components/Nest';
 import { useState } from 'react';
 
-import { Slider } from '@mui/material';
+import { Slider, Select, MenuItem } from '@mui/material';
 
 function App() {
   const [depth, setDepth] = useState(40);
   const [scale, setScale] = useState(0.8);
   const [speed, setSpeed] = useState(40);
   const [size, setSize] = useState(25);
+  const [shape, setShape] = useState('square');
+  const [inputDisplay, setInputDisplay] = useState(true);
 
   const handleDepthChange = (newDepth) => {
     setDepth(newDepth);
@@ -26,44 +28,63 @@ function App() {
     setSize(newSize);
   };
 
+  const handleShapeChange = (newShape) => {
+    setShape(newShape);
+  };
+
   return (
     <div>
-      <div style={{ width: '30vw', margin: '2rem auto' }}>
-        <span>Size</span>
-        <Slider
-          min={10}
-          max={50}
-          step={1}
-          value={size}
-          onChange={(e) => handleSizeChange(e.target.value)}
-        />
-        <span>Depth</span>
-        <Slider
-          min={1}
-          max={60}
-          step={1}
-          value={depth}
-          onChange={(e) => handleDepthChange(e.target.value)}
-        />
-        <span>Scale</span>
-        <Slider
-          min={0.7}
-          max={0.9}
-          step={0.01}
-          value={scale}
-          onChange={(e) => handleScaleChange(e.target.value)}
-        />
-        <span>Time to Rotate</span>
-        <Slider
-          min={5}
-          max={50}
-          step={1}
-          value={speed}
-          onChange={(e) => handleSpeedChange(e.target.value)}
-        />
+      <div className="input-container">
+        <div>
+          <span>Size</span>
+          <Slider
+            min={10}
+            max={50}
+            step={1}
+            value={size}
+            onChange={(e) => handleSizeChange(e.target.value)}
+          />
+          <span>Depth</span>
+          <Slider
+            min={1}
+            max={60}
+            step={1}
+            value={depth}
+            onChange={(e) => handleDepthChange(e.target.value)}
+          />
+          <span>Scale</span>
+          <Slider
+            min={0.7}
+            max={0.9}
+            step={0.01}
+            value={scale}
+            onChange={(e) => handleScaleChange(e.target.value)}
+          />
+          <span>Time to Rotate</span>
+          <Slider
+            min={5}
+            max={50}
+            step={1}
+            value={speed}
+            onChange={(e) => handleSpeedChange(e.target.value)}
+          />
+        </div>
+        <Select
+          value={shape}
+          onChange={(e) => handleShapeChange(e.target.value)}
+        >
+          <MenuItem value={'square'}>Square</MenuItem>
+          <MenuItem value={'ellipse'}>Ellipse</MenuItem>
+        </Select>
       </div>
       <div className="centered">
-        <Nest depth={depth} size={size} speed={speed} scale={scale} />
+        <Nest
+          depth={depth}
+          size={size}
+          speed={speed}
+          scale={scale}
+          shape={shape}
+        />
       </div>
     </div>
   );
--- src/components/Nest.jsx
+++ src/components/Nest.jsx
@@ -1,21 +1,36 @@
 import './nest.css'
-export const Nest = ({ depth, size, speed, scale }) => {
+export const Nest = ({ depth, size, speed, scale, shape }) => {
 
   if (depth < 1) return null;
 
-  const style = {
+  const square = {
     width: `${size}rem`,
     height: `${size}rem`,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     animation: `spin ${speed}s linear infinite`, 
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    transition: 'border-radius 1s, width 1s, height 1s',
   }
 
+  const ellipse = {
+    width: `${size}rem`,
+    height: `${size / 2}rem`,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    animation: `spin ${speed}s linear infinite`, 
+    backgroundColor: 'white',
+    borderRadius: '50%',
+    transition: 'border-radius 1s, width 1s, height 1s',
+
+  }
+
+
   return (
-    <div style={style}>
-      <Nest depth={depth -= 1} size={size *= scale} speed={speed} scale={scale}/>
+    <div style={shape === 'square' ? square : ellipse}>
+      <Nest depth={depth -= 1} size={size *= scale} speed={speed} scale={scale} shape={shape}/>
     </div>
   );
 }
